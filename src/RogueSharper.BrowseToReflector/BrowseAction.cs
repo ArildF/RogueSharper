3366668ad79492e00f1fc4ed847c3028aa0d02f4
Arild <arild.fines@broadpark.no>
2009-06-29 18:48:27 +0200

    Gotten Resharper plugin to load and run, use WCF service to communicate with Reflector

---
+++ src/RogueSharper.BrowseToReflector/BrowseAction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using JetBrains.ActionManagement;
+using RogueSharper.BrowseToReflector.Reflector;
+
+namespace RogueSharper.BrowseToReflector
+{
+    [ActionHandler(new string[] { "RogueSharper.BrowseToReflector.BrowseAction" })]
+    class BrowseAction : IActionHandler
+    {
+        public bool Update(IDataContext context, ActionPresentation presentation, DelegateUpdate nextUpdate)
+        {
+            return true;
+        }
+
+        public void Execute(IDataContext context, DelegateExecute nextExecute)
+        {
+            var binding = new NetNamedPipeBinding(NetNamedPipeSecurityMode.None);
+
+            var endpoint =
+                new EndpointAddress(
+                    "net.pipe://localhost/ReflectorBrowseService");
+
+            var client =
+               new ReflectorBrowseServiceClient(binding, endpoint);
+
+            client.Browse(
+                this.GetType().Assembly.ManifestModule.FullyQualifiedName,
+                this.GetType().FullName,
+                "Execute");
+
+            nextExecute();
+        }
+    }
+}
--- src/RogueSharper.BrowseToReflector/Properties/AssemblyInfo.cs
+++ src/RogueSharper.BrowseToReflector/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using JetBrains.ActionManagement;
+using JetBrains.UI.Application.PluginSupport;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -34,3 +36,10 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+[assembly: PluginDescription("A plugin for browsing to Reflector")]
+[assembly: PluginTitle("RogueSharper.BrowseToReflector")]
+[assembly: PluginVendor("Me")]
+
+[assembly: ActionsXml("RogueSharper.BrowseToReflector.Actions.xml", Precompile = false)]
