a5c20dfb0e495406964e3771d56ad855424ef27e
Arild <arild.fines@broadpark.no>
2009-07-01 12:36:19 +0200

    Unsaved file...

--- src/RogueSharper.BrowseToReflector/ElementFinder.cs
+++ src/RogueSharper.BrowseToReflector/ElementFinder.cs
@@ -43,20 +43,4 @@ namespace RogueSharper.BrowseToReflector
 
         }
     }
-
-    internal class Element
-    {
-        public static readonly Element NotFound = new Element("", "", "");
-
-        public string AssemblyFile { get; private set; }
-        public string TypeName { get; private set; }
-        public string MemberName { get; private set; }
-
-        public Element(string assemblyFile, string typeName, string memberName)
-        {
-            AssemblyFile = assemblyFile;
-            TypeName = typeName;
-            MemberName = memberName;
-        }
-    }
 }
