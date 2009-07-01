7a8ecb94541a29b44b143f994450525cd10ff9c1
Arild <arild.fines@broadpark.no>
2009-07-02 01:39:28 +0200

    Emit a full .cfg file for Reflector

--- src/RogueSharper.BrowseToReflector/ReflectorConfigFile.cs
+++ src/RogueSharper.BrowseToReflector/ReflectorConfigFile.cs
@@ -43,8 +43,33 @@ namespace RogueSharper.BrowseToReflector
             string reflectorPlugin = System.IO.Path.Combine(currentAssemblyDir,
                                                             "RogueSharper.ReflectorBrowseServicePlugin.dll");
             
-            string contents = String.Format(@"[AddInManager]
-""{0}""",
+            string contents = String.Format(@"
+[AddInManager]
+""{0}""
+
+[AssemblyCache]
+""%SystemRoot%\Microsoft.net""
+""%ProgramFiles%\Reference Assemblies""
+""%ProgramFiles%\Microsoft.net""
+""%ProgramFiles%\Microsoft Silverlight""
+
+[AssemblyManager]
+""C:\Windows\Microsoft.NET\Framework\v2.0.50727\Microsoft.Build.Engine.dll""
+""%SystemRoot%\Microsoft.net\Framework\v2.0.50727\mscorlib.dll""
+""%SystemRoot%\Microsoft.net\Framework\v2.0.50727\System.dll""
+""%SystemRoot%\Microsoft.net\Framework\v2.0.50727\System.Xml.dll""
+""%SystemRoot%\Microsoft.net\Framework\v2.0.50727\System.Data.dll""
+""%SystemRoot%\Microsoft.net\Framework\v2.0.50727\System.Web.dll""
+""%SystemRoot%\Microsoft.net\Framework\v2.0.50727\System.Drawing.dll""
+""%SystemRoot%\Microsoft.net\Framework\v2.0.50727\System.Windows.Forms.dll""
+""%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.5\System.Core.dll""
+""%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.0\System.ServiceModel.dll""
+""%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.0\System.Workflow.ComponentModel.dll""
+""%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.0\System.Workflow.Runtime.dll""
+""%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.0\System.Workflow.Activities.dll""
+""%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.0\WindowsBase.dll""
+""%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.0\PresentationCore.dll""
+""%ProgramFiles%\Reference Assemblies\Microsoft\Framework\v3.0\PresentationFramework.dll""",
                                             reflectorPlugin);
 
             File.WriteAllText(_path, contents);
